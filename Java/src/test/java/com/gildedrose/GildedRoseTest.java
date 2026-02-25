d6af0b49a04856705372158db422ce07e80d0ac4
Seppe Berghmans <seppe.berghmans@vdab.be>
2026-02-25 15:29:15 +0100

    default test for quality all items

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -16,4 +16,24 @@ class GildedRoseTest {
         assertEquals(9, item.sellIn);
         assertEquals(19, item.quality);
     }
+
+    @Test
+    void allItems_qualityNeverNegative() {
+        Item item = new Item("Normal Item", 10, 0);
+        GildedRose app = new GildedRose(new Item[] { item });
+
+        app.updateQuality();
+        assertEquals(9, item.sellIn);
+        assertEquals(0, item.quality);
+    }
+
+    @Test
+    void allItems_qualityNeverExceedsFifty() {
+        Item item = new Item("Aged Brie", 10, 50);
+        GildedRose app = new GildedRose(new Item[] { item });
+
+        app.updateQuality();
+        assertEquals(9, item.sellIn);
+        assertEquals(50, item.quality);
+    }
 }
