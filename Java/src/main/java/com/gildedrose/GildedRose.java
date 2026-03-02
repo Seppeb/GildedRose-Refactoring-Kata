7199ffac216b8c5711be611831eb60c1fca84d7c
Seppe Berghmans <seppe.berghmans@vdab.be>
2026-03-02 10:07:48 +0100

    private methods for updateQuality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -24,20 +24,22 @@ class GildedRose {
     private void updateQuality(Item item) {
         if (item.name.equals(AGED_BRIE)
             || item.name.equals(BACKSTAGE_PASSES)) {
-                if (item.quality < 50) {
+            updateQualityAgedBriedAndBackstagePasses(item);
+        } else if (item.quality > 0 && !item.name.equals(SULFURAS)) {
+            decreaseQuality(item);
+        }
+    }
+
+    private void updateQualityAgedBriedAndBackstagePasses(Item item) {
+        if (item.quality < 50) {
+            increaseQuality(item);
+            if (item.name.equals(BACKSTAGE_PASSES)) {
+                if (item.sellIn < 11 && item.quality < 50) {
                     increaseQuality(item);
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.sellIn < 11 && item.quality < 50) {
-                            increaseQuality(item);
-                        }
-                        if (item.sellIn < 6 && item.quality < 50) {
-                            increaseQuality(item);
-                        }
-                    }
                 }
-            } else {
-            if (item.quality > 0 && !item.name.equals(SULFURAS)) {
-                decreaseQuality(item);
+                if (item.sellIn < 6 && item.quality < 50) {
+                    increaseQuality(item);
+                }
             }
         }
     }
